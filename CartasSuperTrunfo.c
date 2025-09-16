bbe8fb2838b89988d3d84a9cfd9c8d613eff2170
heldercb <hdr.cruz@outlook.com>
2025-09-16 23:32:12 +0000

    feature: ler e imprimir dados de cartas de duas cidades

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,46 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  int populacao_1, populacao_2, numero_pontos_turisticos_1, numero_pontos_turisticos_2;
+  float area_1, area_2, pib_1, pib_2;
 
   // Área para entrada de dados
+  printf("Digite a população da primeira carta:\n");
+  scanf("%d", &populacao_1);
+
+  printf("Digite a área da primeira carta:\n");
+  scanf("%f", &area_1);
+
+  printf("Digite o pib da primeira carta:\n");
+  scanf("%f", &pib_1);
+
+  printf("Digite o número de pontos turísticos da primeira carta:\n");
+  scanf("%d", &numero_pontos_turisticos_1);
+
+  printf("Digite a população da segunda carta:\n");
+  scanf("%d", &populacao_1);
+
+  printf("Digite a área da segunda carta:\n");
+  scanf("%f", &area_1);
+
+  printf("Digite o pib da segunda carta:\n");
+  scanf("%f", &pib_1);
+
+  printf("Digite o número de pontos turísticos da segunda carta:\n");
+  scanf("%d", &numero_pontos_turisticos_1);
 
   // Área para exibição dos dados da cidade
+  printf("CARTA 1\n");
+  printf("População: %d \n", populacao_2);
+  printf("Área: %f \n", area_2);
+  printf("PIB: %f \n", pib_2);
+  printf("Pontos Turísticos: %d \n", numero_pontos_turisticos_2);
+
+  printf("CARTA 2\n");
+  printf("População: %d \n", populacao_2);
+  printf("Área: %f \n", area_2);
+  printf("PIB: %f \n", pib_2);
+  printf("Pontos Turísticos: %d \n", numero_pontos_turisticos_2);
 
 return 0;
 } 
